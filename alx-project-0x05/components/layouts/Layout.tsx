651ec57c250a4c6ed2a5633bdc934a27266a8754
poseitutu10 <phinehasoseitutu1@gmail.com>
2025-06-12 12:55:13 +0100

    context api in nextjs

---
+++ alx-project-0x05/components/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import { LayoutProps } from "@/interface";
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
---
+++ alx-project-0x05/context/CountContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+interface CountContextProps {
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}
+export const Context = createContext<CountContextProps | undefined>(undefined);
+
+export const CountContext = ({ children }: { children: ReactNode }) => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => setCount((count) => count + 1);
+
+  const decrement = () => setCount((count) => (count > 0 ? count - 1 : 0));
+
+  return <Context.Provider value={{count, increment, decrement}}>{children}</Context.Provider>;
+};
+
+
+export const useCount = () => {
+    const countObject = useContext(Context);
+
+    if(!countObject){
+        throw new Error("useCount must be within context provider")
+    }
+
+    return countObject;
+}
